f72365c6c46195bd4efc925c04330a5dec6583ff
gta0814 <chinagengweite@hotmail.com>
2017-09-07 14:46:23 -0600

    :100:

---
+++ Extension Methods/Quackers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quackers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Weite Geng";
+
+            Console.WriteLine(name);
+            Console.WriteLine(name.Quack());
+        }
+    }
+
+    public static class Extensions // Note that this is STATIC
+    {
+        public static string Quack(this string self)
+        {
+            return self + " (quack) ";
+        }
+    }
+}
