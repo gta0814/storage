f013d70dbac18491fa1c68be62266c536f5967bf
weite <chinagengweite@hotmail.com>
2019-12-05 15:08:51 -0700

    ...

--- C#/test/test/Program.cs
+++ C#/test/test/Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Utility;
 
 namespace test
 {
-    class Program
+    class Program : calculation
     {
         static void Main(string[] args)
         {
-            var pw = "FP8NEsilUH19BzSvmSb8Yw==";
-            var d = "doie321";
+            //var pw = "FP8NEsilUH19BzSvmSb8Yw==";
+            //var d = "doie321";
 
-            Console.WriteLine(pw.GetType().GetProperty);
+            //Console.WriteLine(pw.GetType().GetProperty);
+            var a = new nulber();
+            a.print();
             Console.ReadKey();
         }
+    }
+    class calculation
+    {
+        public double add (double a, double b)
+        {
+            return a + b;
+        }
+    }
+    class nulber : calculation
+    {
+        public void print()
+        {
+            Console.WriteLine(base.add(1, 2));
+        }
+        
     }
 }
