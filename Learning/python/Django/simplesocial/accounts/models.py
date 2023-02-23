de0b68f5d60beb6d8cd27bc8461b9fed4529d080
GTA0814 <gdt0814@gmail.com>
2023-02-22 23:17:04 -0700

    simplesotical from udemy

---
+++ Learning/python/Django/simplesocial/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib import auth
+
+# Create your models here.
+class User(auth.get_user_model(), auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
